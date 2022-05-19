95ce579745a0a273744dcf5905b465ed880a8a76
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-19 09:56:43 -0600

    fix(rc4): trame.widgets initialize

--- trame/widgets/matplotlib.py
+++ trame/widgets/matplotlib.py
@@ -4,4 +4,4 @@ from trame_matplotlib.widgets.matplotlib import *
 def initialize(server):
     from trame_matplotlib import module
 
-    server.enable_module(server)
+    server.enable_module(module)
